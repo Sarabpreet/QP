d7b635aa51041296892ff5eee38bcdea15fe4c9d
Sarabpreet Singh <Sarabpreetsingh23@gmail.com>
2016-01-28 15:06:38 +0530

    rp++

---
+++ CredrUX-Quick Presentation.php
@@ -0,0 +1,20 @@
+<?php 
+include 'pagebuilder/header.php'; 
+ ?>
+
+<section>
+
+<h2>hi this is heading...</h2>
+	Blah Blah
+
+</section>
+
+<section>
+	Blah Blah
+</section>
+
+
+
+ <?php 
+include 'pagebuilder/footer.php'; 
+ ?>
