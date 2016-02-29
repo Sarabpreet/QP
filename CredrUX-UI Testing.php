a899b201d2b8b9d6614d587ac57434d25d73bf2e
Sarabpreet Singh <Sarabpreetsingh23@gmail.com>
2016-02-29 19:15:09 +0530

    changess++

---
+++ CredrUX-UI Testing.php
@@ -0,0 +1,161 @@
+<?php 
+include 'pagebuilder/header.php'; 
+ ?>
+
+
+
+<section>
+	
+
+<h1>UI Testing</h1>
+
+<p>Random stuffs :') </p>
+
+
+</section>
+
+<section>
+<h5>Problems in 1440</h5>
+
+
+			<section>
+							<img src="img/ui-testing/1440-1.png" alt="">
+							<p>
+							Gap in the banner on right
+
+							</p>
+
+			</section>
+
+
+			<section>
+
+
+							<img src="img/ui-testing/1440-2.png" alt="">	
+							<p>Font spacing in location</p>
+			</section>
+
+
+			<section>
+
+
+							<img src="img/ui-testing/1440-3.png" alt="">	
+							<p>Listing page breaks after closing rating section.</p>
+			</section>
+
+
+			<section>
+
+
+							<img src="img/ui-testing/1440-4.png" alt="">	
+							<p>Ther are no suggestions given on typing the brand/make of the bike.</p>
+			</section>
+
+
+			<section>
+
+
+							<img src="img/ui-testing/1400-5.png" alt="">	
+							<p>Triangle get lost after selecting something from dropdown.</p>
+			</section>
+
+
+
+</section>
+<!-- 1440 ends here -->
+
+<section>
+	
+<h5>Problems in 1366 Mozilla</h5>
+
+
+					<section>
+						<img src="img/ui-testing/1366-1.png" alt="">	
+						<p>Font size used here is 14,  we have to use size 12</p>
+					</section>
+				
+					
+					<section>
+						<img src="img/ui-testing/1366-2.png" alt="">	
+						<p>Flag is not alligned to top, The font weights are varying from origional</p>
+					</section>
+				
+
+
+					<section>
+						<img src="img/ui-testing/1366-3.png" alt="">	
+						<p>Remove lines, except for the select option.</p>
+					</section>
+				
+							<section>
+						<img src="img/ui-testing/1366-4.png" alt="">	
+						<p>Align the arrow with sort.</p>
+					</section>
+				
+
+							<section>
+						<img src="img/ui-testing/1366-5.png" alt="">	
+						<p>There is no arrow for dropdown after selecting,  Also align rating and sort.</p>
+					</section>
+				
+
+
+							<section>
+						<img src="img/ui-testing/1366-6-win.png" alt="">	
+						<p>Icons are different from the given design, also Increase the opacity of sub heading (lorem ipsum)  ,  Make the button's text bold (SHOW BIKES),   CTA and search bar should be rounded rectangles, Today's offer text should be bold,   Filters and categories and other filters should be bold.</p>
+					</section>
+
+
+
+							<section>
+						<img src="img/ui-testing/1366-7-win.png" alt="">	
+						<p>Increase the distance between check box and text in view all models</p>
+					</section>
+
+
+
+							<section>
+						<img src="img/ui-testing/1366-8.png" alt="">	
+						<p>"forgot password?" - should be 12px, Keeping track line should be bold, button font should be 12px bold, "Don't have an account yet?" should be 14 px not 16px and REGISTER is 16px</p>
+					</section>
+
+
+
+
+</section>
+
+
+
+<!-- 1366 mozilla problem -->
+ <?php 
+include 'pagebuilder/footer.php'; 
+
+ ?>
+
+
+
+
+
+<style>
+	
+
+section img {
+
+max-height: 300px;
+
+
+	}
+
+section p {
+font-size: .5em;
+}
+
+.reveal h5 { font-size: 1em; } 
+
+
+.reveal p { font-size: .4em; } 
+
+
+</style>
+
+
--- rp/js/custom.js
+++ rp/js/custom.js
@@ -1,46 +1,29 @@
-  smoothScroll.init();
+smoothScroll.init();
 
-  (function($) {
-  $(document.body).fadeIn(1000);
+
+
+(function($) {
+$(document.body).fadeIn(1000);
 })(jQuery);
-
-
-
 //#bike, #inspeC, #inspecR,#feature{
+var creep=document.querySelectorAll(".creep li");
 
-  var creep=document.querySelectorAll(".creep li");
- 
- for (var i = 0; i < creep.length; i++) {
-    creep[i].addEventListener('click',creepcall);
- };
+for (var i = 0; i < creep.length; i++) {
+creep[i].addEventListener('click',creepcall);
+};
+function creepcall(){
 
-  function creepcall(){
-
-
- 
-  var val=($(this).text());
-console.log($(this).text());
-   hideExcept(val);
-  }
-
-
+var val=($(this).text());
+hideExcept(val);
+}
 function hideExcept(abc){
-
-
 for (var i = 0; i <creep.length; i++) {
- 
+
 if(creep[i].textContent==abc) {
 creep[i].className='active';
-
-} 
-else {
-
-  creep[i].className="";
 }
-
-
+else {
+creep[i].className="";
+}
 };
-
-
-
 }
